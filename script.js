20c19e34091855e34b9f696c872e7d593303ebae
msalesforce <107444573+msalesforce@users.noreply.github.com>
2022-06-19 17:11:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,135 @@
-// Global Constants
-const apiKey = '03a0bef92ef5c5949f333c1f4de9ffbf';
-const pageSize = 12;
+/* Global Constants */
+const apiKey = ''
+const baseUrl = 'https://api.themoviedb.org/3/'
+const imageUrl = 'https://image.tmdb.org/t/p/original'
+
+/* Page Elements
+Key: use querySelector in place of getElementByID for more versatility in CSS */
+const searchbar = document.querySelector('#search-bar')
+const query = document.querySelector('#search-input')
+const queryTitle = document.querySelector('#search-title')
+const movieArea = document.querySelector('#movies-grid')
+const nowPlaying = document.querySelector('#now-playing')
+
+const showMeMoreBtn = document.querySelector('#load-more-movies-btn')
+const exitSearchBtn = document.querySelector('#exit-search-btn')
 
 // Global Variables
-var currApiPage = 0;
-var currSearchTerm = '';
+let currApiPage = 1;
+let currQuery = ''
 
-// Page Elements
-const searchForm = document.getElementById('search-form');
-const searchInput = document.getElementById('search-input');
-const movieAreaDiv = document.getElementById('movie-area');
-const showMeMoreBtn = document.getElementById('show-me-more-btn');
+/* Listen for separate user actions */
+searchbar.addEventListener('submit', handleFormSubmit)
+exitSearchBtn.addEventListener('click', exitSearch)
+showMeMoreBtn.addEventListener('click', showMeMore)
 
-/** Get results from API. */
-async function getData() {
-    url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`
+/* Allow JS to wait while API retrieves data */
+window.onload = displayCurrentMovies
 
-    //imgUrl = 'https://api.themoviedb.org/3/search/movie/?movie_id=${movie_id}/images?api_key=${apiKey}'
-    var data = await fetch(url);
-    var jsonData = await data.json();
-    console.log(jsonData);
-    // return jsonData.data;
-    displayMovies(data.results);
-}
-console.log(getData())
-
-
-/** Render list of results.  */
-function displayMovies(movies) {
-    movies.forEach(function(movie) {
-        movieAreaDiv.innerHTML =
-        `<div class="img">
-            <img src="${movie_id}" />
-        </div>
-    `).join('');
-    movieAreaDiv.innerHTML += imgHTMLString;
-    })
-    // movieAreaDiv.innerHTML = data.forEach(img => `
-    //     <div class="img">
-    //         <img src="${movie_id}" />
-    //     </div>
-    // `).join('');
-    // movieAreaDiv.innerHTML += imgHTMLString;
-}
-
-function getMovieTemplate(movie) {
-    return `<image src=${movie.img} />
-            <span>${movie.name}</span>`
-}
-
-/** On form submit, get results and add to list. */
-async function handleFormSubmit(event) {
+/* Gets currently playing movies */
+async function displayCurrentMovies(event) {
     event.preventDefault();
-    movieAreaDiv.innerHTML = '';
-    currSearchTerm = searchInput.value;
-    const results = await getData(currSearchTerm);
-    displayResults(results);
-    searchInput.value = '';
-    currApiPage++;
-    showMeMoreBtn.classList.remove('hidden');
-}
+    let response = await fetch(baseUrl + 'movie/now_playing?api_key=' + apiKey + '&language=en-US&page=' + currApiPage);
+    console.log(response);
 
-searchForm.addEventListener('submit', handleFormSubmit);
+    let responseData = await response.json();
+    console.log(responseData);
 
-async function handleShowMeMoreClick(event) {
-    const results = await getData(currSearchTerm);
-    displayResults(results);
+    displayResults(responseData.results);
+    addEventListenersToCards();
+
     currApiPage++;
 }
 
-showMeMoreBtn.addEventListener('click', handleShowMeMoreClick);
+ /* On form submit, get results and add to list. */
+async function searchResults() {
+    currQuery = query.value
+
+    // Display only titles relevant to search
+    queryTitle.classList.remove('hidden')
+    exitSearchBtn.classList.remove('hidden')
+    queryTitle.innerHTML = `
+    Showing all titles with: "${currQuery}"
+    `
+    nowPlaying.classList.add('hidden')
+
+    let response = await fetch(baseUrl + 'search/movie?api_key=' + apiKey + '&language=en-US&query=' + currQuery + '&page=' + currApiPage + '&include_adult=false')
+    let responseData = await response.json()
+
+    if (responseData.results.length != 0) {  // display relevant movies as available
+        displayResults(responseData.results)
+    }
+    else if (currApiPage == 1) {  
+        movieArea.innerHTML = `
+        <h2>No results for</h2>
+        <h2>"${currQuery}"</h2>
+        <h2>No movies with the search term '${currQuery}' found.</h2>`
+        showMeMoreBtn.classList.add('hidden')
+    }
+
+    else {
+        movieArea.innerHTML += `
+        <div>
+        <h2>No more results for </h2>
+        <h2>"${currQuery}."</h2>
+        </div>`
+        showMeMoreBtn.classList.add('hidden')
+    }
+    addEventListenersToCards();
+    currApiPage++;
+
+}
+
+function displayResults(results) {
+    // Adds each movie in the array
+    results.forEach((movie, index) => {
+        movieArea.innerHTML += `
+        <div class='movie-card' id='${((currApiPage - 1) * 20) + index}'>
+            <img src=${imageUrl + movie.poster_path} class=movie-poster alt='${movie.original_title}' title='${movie.original_title}' width=100%>
+            <p class='movie-votes'>⭐️ ${movie.vote_average}</p>
+            <p class='movie-title'>${movie.title}</p>
+        </div>
+        `
+    })
+}
+
+/* Handles search form entries */
+function handleFormSubmit(event) {
+    event.preventDefault()
+    if (movieArea) {  // display search results on clean page
+        movieArea.innerHTML = ''
+    }
+    currApiPage = 1;
+    searchResults()
+}
+
+/* Exits search and reverts to landing page */
+function exitSearch(event) {
+    queryTitle.classList.add('hidden')  // Revert to current titles
+    exitSearchBtn.classList.add('hidden')
+    nowPlaying.classList.remove('hidden')
+
+    movieArea.innerHTML = '' // reset page
+    query.value = ''
+    currApiPage = 1;
+
+    displayCurrentMovies(event)
+}
+
+/* Shows move movies! */
+function showMeMore(event) {
+    if (!(queryTitle.classList.contains('hidden'))) {  // query-specific
+        searchResults()
+    } 
+    else {
+    displayCurrentMovies(event)
+    }
+}
+
+/* Add event listener to movie card */
+function addEventListenersToCards() {
+    const movieCardQS = document.querySelectorAll(".movie-card")
+    movieCardQS.forEach(card => {
+        card.addEventListener("click", () => generateCard(card))
+    })
+}
